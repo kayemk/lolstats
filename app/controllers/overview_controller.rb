5a3876d78bc661c0b3536afad57e1ac0607eed2c
kayemk <contact@kayemk.net>
2017-08-19 23:27:13 +0200

    Changed library for api client to faraday.

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -1,33 +1,24 @@
-require 'net/http'
+require 'faraday'
 require 'dotenv/load'
-#require 'json'
 
 class OverviewController < ApplicationController
-  #default_timeout 1 # hard timeout after 1 second
+
   def api_key
     ENV["LOLSTATS_API_KEY"]
-    @test_debug = ENV["LOLSTATS_API_KEY"]
   end
 
-  def handle_timeouts
-    begin
-      yield
-    rescue Net::OpenTimeout, Net::ReadTimeout
-      {}
-    end
+  def url
+    "https://euw1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US&tags=stats&dataById=false&api_key="+api_key
   end
-
 
   def index
-    handle_timeouts do
-      url = "https://na1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US&dataById=false&api_key=#{ api_key }"
-      uri = URI(url)
-      response = Net::HTTP.get(uri) 
-      @url_debug = url
-      @my_hash = response
+
+    conn = Faraday.new(url, request: {open_timeout: 1, timeout: 1}) do |c|
+      c.use Faraday::Response::Logger     # log request & response to STDOUT
+      c.use Faraday::Adapter::NetHttp     # perform requests with Net::HTTP
     end
+  response = conn.get url
+  @my_hash = response.body
   end
 
-  #def 
-  #end
 end
