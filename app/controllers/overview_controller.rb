e0b44a183a50bef9307dd180473818e7e95b60a6
kayemk <contact@kayemk.net>
2017-09-11 23:32:28 +0200

    Added skeleton boilerplate. Still testing api calls and figuring out how to save data for my usage.

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -1,5 +1,6 @@
 require 'faraday'
 require 'dotenv/load'
+require 'faraday_middleware'
 
 class OverviewController < ApplicationController
 
@@ -14,12 +15,23 @@ class OverviewController < ApplicationController
   def index
 
     conn = Faraday.new(url, request: {open_timeout: 1, timeout: 1}) do |c|
+      c.use FaradayMiddleware::ParseJson,       content_type: 'application/json'
       c.use Faraday::Response::Logger     # log request & response to STDOUT
       c.use Faraday::Adapter::NetHttp     # perform requests with Net::HTTP
     end
 
   response = conn.get url
   @my_hash = response.body
+  # Jax
+  @champs_name = response.body['data']['Jax']['name']
+  @champs_armor = response.body['data']['Jax']['stats']['armor']
+  @champs_armorperlevel = response.body['data']['Jax']['stats']['armorperlevel']
+  @champs_attackdamage = response.body['data']['Jax']['stats']['attackdamage']
+  # Annie
+  @champss_name = response.body['data']['Annie']['name']
+  @champss_armor = response.body['data']['Annie']['stats']['armor']
+  @champss_armorperlevel = response.body['data']['Annie']['stats']['armorperlevel']
+  @champss_attackdamage = response.body['data']['Annie']['stats']['attackdamage']
   end
 
 end
